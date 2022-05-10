510e97b8f845be7d6fbd7415f7530e7b845a8167
eneridahunda <eneridahunda@gmail.com>
2022-05-10 16:33:19 +0200

    Uppgift 1:public class Uppgift 2: static

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+ public class Demo
+{
+    static void Main()
+    {
+        string name;
+        Console.Write("Enter name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Your input: {0}", name);
+
+        string a;
+        Console.WriteLine("Enter salary : ");
+        a = Console.ReadLine();
+        Console.WriteLine("Your salary:{0}", a);
+
+        
+    }
+}
